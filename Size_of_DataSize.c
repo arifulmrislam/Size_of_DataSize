2db9b04c25a101c53fffef6779a8d1767f135305
arifulmrislam <ariful.mr.islam@gmail.com>
2021-10-25 21:06:41 +0300

    DataSize

---
+++ Size_of_DataSize.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Size of Char data type = %d\n",sizeof(char));
+    printf("Size of Short data type = %d\n",sizeof(short));
+    printf("Size of int data type = %d\n",sizeof(int));
+    printf("Size of long data type = %d\n",sizeof(long));
+    printf("Size of long long data type = %d\n",sizeof(long long));
+    return 0;
+}
